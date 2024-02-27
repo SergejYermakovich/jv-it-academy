92d01fdbf0a5b35354afcd67550a124c4c3a8e46
SergejYermakovich <sergu9848@gmail.com>
2024-02-27 19:08:59 +0300

    lesson 24 update

---
+++ src/lesson24/PalindromeTest.java
@@ -0,0 +1,24 @@
+package lesson24;
+
+public class PalindromeTest {
+    public static void main(String[] args) {
+        // "Хил, худ, а дух лих." -> "хил, худ, а дух лих." -> "хилхудадухлих"
+
+        System.out.println(isPalindrome("Хил, худ, а дух лих.") == true);
+        System.out.println(isPalindrome("1Хил, а дух лих.") == true);
+    }
+
+    private static boolean isPalindrome(String checkString) {
+        String lowerCaseString = checkString.toLowerCase();
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < lowerCaseString.length(); i++) {
+            char currentSymbol = lowerCaseString.charAt(i);
+            if (Character.isAlphabetic(currentSymbol) || Character.isDigit(currentSymbol)) {
+                result.append(currentSymbol);
+            }
+        }
+
+        return result.reverse().toString().equals(result.toString());
+    }
+}
---
+++ src/lesson24/RansomNote.java
@@ -0,0 +1,2 @@
+package lesson24;public class RansomNote {
+}
